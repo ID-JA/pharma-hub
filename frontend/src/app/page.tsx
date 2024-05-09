837cf6ee62ee3e5401b46cbe2c9496c17885ea24
nazhahaida <nazhahaida1@gmail.com>
2024-05-09 14:09:21 +0200

    setup layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <h1>
       PharmaHub Next Gen for pharmacy management
-      <Link href="/home">home</Link>
+      <Link href="/portal">home</Link>
       <Button>CLICK</Button>
     </h1>
   );
---
+++ frontend/src/app/portal/layout.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { AppShell, Burger, Text } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { PortalNavbar } from "@/components/navbar/navbar";
+
+function PortalLayout({ children }) {
+  const [opened, { toggle }] = useDisclosure();
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        <Text>PharmaHub</Text>
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">
+        <PortalNavbar />
+      </AppShell.Navbar>
+
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
+}
+
+export default PortalLayout;
