3d500451a0587c91166fa7b567d4cc27fa21a12d
ID-JA <idaissa93@gmail.com>
2024-05-10 21:39:49 +0100

    add default datetime for records

--- PharmaHub.API/Models/BaseModel.cs
+++ PharmaHub.API/Models/BaseModel.cs
@@ -2,6 +2,6 @@ namespace PharmaHub.API.Models;
 public class BaseModel
 {
     public int Id { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; } 
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
 }
