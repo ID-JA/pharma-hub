b397e2d4592eeab74e51509c9c606e5df917f0d1
ID-JA <idaissa93@gmail.com>
2024-05-12 21:57:28 +0100

    feat(api): add a generic service to support all entitie for crud operations

---
+++ PharmaHub.API/Services/BaseService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PharmaHub.API.Services.Interfaces;
+
+namespace PharmaHub.API.Services;
+
+public class BaseService<T>(ApplicationDbContext dbContext) : IBaseService<T> where T : class
+{
+    public async Task<T> AddAsync(T entity, CancellationToken cancellationToken)
+    {
+        var result = await dbContext.Set<T>().AddAsync(entity, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
+        return result.Entity;
+    }
+
+    public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default)
+    {
+        await dbContext.Set<T>().AddRangeAsync(entities, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
+        return entities;
+    }
+
+    public async Task DeleteAsync(T entity, CancellationToken cancellationToken = default)
+    {
+        dbContext.Set<T>().Remove(entity);
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task DeleteRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default)
+    {
+        dbContext.Set<T>().RemoveRange(entities);
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<List<T>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await dbContext.Set<T>().ToListAsync(cancellationToken);
+    }
+
+    public async Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        return await dbContext.Set<T>().FindAsync([id], cancellationToken: cancellationToken);
+    }
+
+
+    public async Task UpdateAsync(T entity, CancellationToken cancellationToken = default)
+    {
+        dbContext.Set<T>().Update(entity);
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
