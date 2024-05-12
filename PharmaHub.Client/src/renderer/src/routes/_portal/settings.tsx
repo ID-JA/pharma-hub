b44f4f7c12b359269b149e0466fe273bbb755d26
ID-JA <idaissa93@gmail.com>
2024-05-12 14:32:57 +0100

    reuse the tabs header

--- PharmaHub.Client/src/renderer/src/routes/_portal/settings.tsx
+++ PharmaHub.Client/src/renderer/src/routes/_portal/settings.tsx
@@ -1,12 +1,23 @@
 import { Box } from '@mantine/core'
-import { SettingsHeader } from '@renderer/components/SettingsHeader'
+import { TabsHeader } from '@renderer/components/TabsHeader'
 import { Outlet } from '@tanstack/react-router'
 import { createFileRoute } from '@tanstack/react-router'
 
+const links = [
+  {
+    to: '/settings',
+    label: 'Summary',
+    exact: true
+  },
+  {
+    to: '/settings/users',
+    label: 'Users management'
+  }
+]
 export const Route = createFileRoute('/_portal/settings')({
   component: () => (
     <div>
-      <SettingsHeader />
+      <TabsHeader links={links} />
       <Box p="md">
         <Outlet />
       </Box>
