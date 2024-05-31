86d29fd279ee34f5956713783c1afca7a73d63ef
nazhahaida <nazhahaida1@gmail.com>
2024-05-31 18:40:55 +0200

    add new columns to Bill entity

--- PharmaHub.API/Models/Bill.cs
+++ PharmaHub.API/Models/Bill.cs
@@ -1,10 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace PharmaHub.API.Models;
 
 public class Bill : BaseModel
 {
     public int BillNumber { get; set; }
-    public string Status { get; set; }
-    public int UserId { get; set; }
-    public User User { get; set; }
-    public List<Order> Orders { get; set; }
+    public DateTime BillDate { get; set; }
+    public string PaymentType { get; set; }
+    public string? CheckNumber { get; set; }
+    public string? EffectNumber { get; set; }
+    public DateTime? DueDate { get; set; }
+    public DateTime? DisbursementDate { get; set; }
+
+    public string? BankName { get; set; }
+    [Precision(10, 2)]
+    public decimal TotalPayment { get; set; }
+
 }
